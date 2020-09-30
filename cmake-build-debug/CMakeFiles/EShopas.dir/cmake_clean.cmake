ff849a3b0f8c0c48093aa9f9aabe35b1c1eaa830
marius <m.gzegozevskis@gmail.com>
2020-09-30 14:31:23 +0300

    Eshop projektas

---
+++ cmake-build-debug/CMakeFiles/EShopas.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EShopas.dir/main.cpp.obj"
+  "EShopas.exe"
+  "EShopas.exe.manifest"
+  "EShopas.pdb"
+  "libEShopas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EShopas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
